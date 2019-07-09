844db87995c49c5169d3a91bf05df29d6548653c
bree <dwf1023@163.com>
2019-07-09 18:06:31 +0800

    bree--

---
+++ spring/src/main/java/com/bree/serviceapi/ServiceApiApplication.java
@@ -0,0 +1,29 @@
+package com.bree.serviceapi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        System.out.println("测试");
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+}
