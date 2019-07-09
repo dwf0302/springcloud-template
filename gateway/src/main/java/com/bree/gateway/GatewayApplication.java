96c130688d35852174bd7b091171e371302f6d46
bree <dwf1023@163.com>
2019-07-09 16:17:33 +0800

    测试idea提交

--- gateway/src/main/java/com/bree/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/bree/gateway/GatewayApplication.java
@@ -31,6 +31,12 @@ public class GatewayApplication {
 
 
 
+
+
+
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
         logger.info(" Start APIGatewayApplication Done");
