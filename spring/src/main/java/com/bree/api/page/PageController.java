17f0863a8043e6c71cc7191442fd76a2a0044147
bree <dwf1023@163.com>
2019-07-11 14:00:15 +0800

    websocket聊天

--- spring/src/main/java/com/bree/api/page/PageController.java
+++ spring/src/main/java/com/bree/api/page/PageController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/index")
+    @GetMapping("/websocket")
     public String index(){
-        return "/index";
+        return "/websocket";
     }
 }
--- spring/src/main/java/com/bree/common/config/websocket/WebSocketConfig.java
+++ spring/src/main/java/com/bree/common/config/websocket/WebSocketConfig.java
@@ -5,15 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
- * 开启WebSocket支持
- * @author zhengkai
+ * @Description: 编写一个WebSocketConfig配置类，注入对象ServerEndpointExporter，
+ * 这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
  */
 @Configuration
 public class WebSocketConfig {
-
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
-
 }
